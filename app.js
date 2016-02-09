2d30f0d69b89fc689344c0097041ce27348c6a59
Noah Laferty <noah.lafferty@gmail.com>
2016-02-08 21:55:22 -0600

    moved some things

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.render('');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
