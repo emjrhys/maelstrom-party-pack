ce06bf3ce7d8b0ce5af57b102896bbf8e60a67a4
Noah Laferty <noah.lafferty@gmail.com>
2016-02-17 21:44:11 -0600

    last for now

--- public/js/faceswap.js
+++ public/js/faceswap.js
@@ -1,16 +1,27 @@
-var top = $('#top-slider').lightSlider({
-	items: 2,
+var sliderSettings = {
+	item: 3,
 	loop: true,
 	controls: false,
-	pager: false
-});
+	pager: false,
+	responsive: [
+		{
+            breakpoint:1100,
+            settings: {
+                item:2
+            }
+        },
+        {
+            breakpoint:700,
+            settings: {
+                item:1
+          	}	
+        }
+	]
+}
 
-var bottom = $('#bottom-slider').lightSlider({
-	items: 2,
-	loop: true,
-	controls: false,
-	pager: false
-});
+var top = $('#top-slider').lightSlider(sliderSettings);
 
-top.goToSlide(Math.floor(Math.random() * 9));
-bottom.goToSlide(Math.floor(Math.random() * 9));
+var bottom = $('#bottom-slider').lightSlider(sliderSettings);
+
+// top.goToSlide(Math.floor(Math.random() * 9));
+// bottom.goToSlide(Math.floor(Math.random() * 9));
